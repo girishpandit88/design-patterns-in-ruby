ec8ba850220e47da0cab56116c74f1197519fd0a
Girish A Pandit <girschol7@gmail.co>
2013-02-02 15:25:30 -0500

    Adding classes to BasicOO

---
+++ BasicOO/Engine.rb
@@ -0,0 +1,8 @@
+class Engine
+  def start
+    puts 'Engine starting...'
+  end
+  def stop
+    puts 'Engine stopping...'
+  end
+end
---
+++ BasicOO/TestDelegation.rb
@@ -0,0 +1,9 @@
+require 'Car'
+
+car = Car.new
+
+car.weekday_drive()
+
+car.switch_to_diesel()
+
+car.weekend_drive()
